636dee649ae38d4a6208a84a2e12c1c23b7af603
unknown <mjlonzame@ymail.com>
2019-12-29 14:55:44 +0400

    added components for assignment

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector : 'app-server',
+	// selector : 'app-server',
+	selector : '[app-server]',
 	templateUrl : './server.component.html'
 })
 export class ServerComponent {
